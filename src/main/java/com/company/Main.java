5ef6514f23bb84819418efc80c2e81186ea0a906
Viktoriia <viktoria.dziuba1997@gmail.com>
2019-04-19 11:59:42 +0300

    Inclused more cases of user input

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-public class Main extends Thread {
+public class Main {
 
     public static void main(String[] args)  {
 
@@ -13,25 +13,27 @@ public class Main extends Thread {
         Scanner sc = new Scanner(System.in);
         String b = "";
 
-        //noinspection AnonymousHasLambdaAlternative
-        Runtime.getRuntime().addShutdownHook(new Thread(){
-            public void run(){
-                try{
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
                     Thread.sleep(200);
                     shortUrl.fillFile();
-                    System.out.println(" " + "There isn't such option");
-                } catch (InterruptedException ex){
+                    System.out.println(" " + "Your app is stopped");
+                } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
             }
         });
 
-        while (!b.equalsIgnoreCase("stop")) {
+        while (true) {
 
+            System.out.println(" ");
             System.out.println("Please choose what you would like to do with your URL:");
-            System.out.println("In case if you want to make your URL shorter, please enter 1");
-            System.out.println("In case if you want to get your original URL by its short version, please enter 2");
-            System.out.println("If you would like to stop the application, please enter 3");
+            System.out.println(" ");
+            System.out.println("   " + "In case if you want to make your URL shorter, please enter 1");
+            System.out.println("   " + "In case if you want to get your original URL by its short version, please enter 2");
+            System.out.println("   " + "If you would like to stop the application, please press CTRL + C");
+            System.out.println(" ");
             System.out.println("Please enter your choice:");
 
             try {
@@ -41,6 +43,7 @@ public class Main extends Thread {
                         System.out.println("Enter your long URL:");
                         String lUrl = sc.next();
                         if (URLValidator.urlValidator(lUrl)) {
+                            System.out.println(" ");
                             System.out.println("This is your short URL:");
                             shortUrl.encode(lUrl);
                         } else {
@@ -51,24 +54,24 @@ public class Main extends Thread {
                     case 2:
                         System.out.println("Enter you short URL");
                         String sUrl = sc.next();
+                        System.out.println(" ");
                         System.out.println("This is your long URL:");
                         longUrl.decode(sUrl);
                         break;
 
-                    case 3:
-                        System.out.println("Just write the word 'stop' ");
-                        b = sc.next();
-                        shortUrl.fillFile();
+                    default:
+                        System.out.println("Please enter one number from 1 to 3");
+                        System.out.println(" ");
 
                 }
 
-            } catch (InputMismatchException e){
-                shortUrl.fillFile();
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter the number of option from the list above");
                 break;
             }
-
-
         }
+
 
     }
 
