7774862a1e16d10d45c82a1eab2d280aed08a313
Viktoriia <viktoria.dziuba1997@gmail.com>
2019-04-22 10:17:11 +0300

    Logback added

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,19 +1,16 @@
 package com.company;
 
-import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class Main {
 
-    static Logger log = Logger.getLogger(Main.class.getName());
+   private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args)  {
-        PropertyConfigurator.configure("src/log4j.properties");
 
         Shortener shortUrl = new Shortener();
         URLDecoder longUrl = new URLDecoder();
@@ -25,7 +22,7 @@ public class Main {
                 try {
                     Thread.sleep(200);
                     shortUrl.fillFile();
-                    log.warning("Your app is stopped");
+                    log.warn("Your app is stopped");
                    // System.out.println(" " + "Your app is stopped");
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
@@ -35,41 +32,41 @@ public class Main {
 
         while (true) {
 
-            log.log(Level.INFO, "Please choose what you would like to do with your URL:");
-            log.log(Level.INFO, "In case if you want to make your URL shorter, please enter 1");
-            log.log(Level.INFO, "In case if you want to get your original URL by its short version, please enter 2");
-            log.log(Level.INFO, "If you would like to stop the application, please press CTRL + C");
-            log.log(Level.INFO,"Please enter your choice:");
+            log.info("Please choose what you would like to do with your URL:");
+            log.info("In case if you want to make your URL shorter, please enter 1");
+            log.info("In case if you want to get your original URL by its short version, please enter 2");
+            log.info("If you would like to stop the application, please press CTRL + C");
+            log.info("Please enter your choice:");
 
             try {
                 int choice = sc.nextInt();
                 switch (choice) {
                     case 1:
-                        log.log(Level.INFO,"Enter your long URL:");
+                        log.info("Enter your long URL:");
                         String lUrl = sc.next();
                         if (URLValidator.urlValidator(lUrl)) {
-                            log.log(Level.INFO,"This is your short URL:");
+                            log.info("This is your short URL:");
                             shortUrl.encode(lUrl);
                         } else {
-                            log.log(Level.INFO,"This URL is not correct");
+                            log.info("This URL is not correct");
                         }
                         break;
 
                     case 2:
-                        log.log(Level.INFO,"Enter you short URL");
+                        log.info("Enter you short URL");
                         String sUrl = sc.next();
-                        log.log(Level.INFO,"This is your long URL:");
+                        log.info("This is your long URL:");
                         longUrl.decode(sUrl);
                         break;
 
                     default:
-                        log.log(Level.INFO,"Please enter one number from 1 to 3");
+                        log.info("Please enter one number from 1 to 3");
 
                 }
 
 
             } catch (InputMismatchException e) {
-                log.warning("Please enter the number of option from the list above");
+                log.warn("Please enter the number of option from the list above");
                 break;
             }
         }
