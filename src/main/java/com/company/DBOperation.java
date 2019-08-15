edfe8f35898747bcdee9ff1dede796d1ffdb7c65
Viktoriia <viktoria.dziuba1997@gmail.com>
2019-08-15 16:33:30 +0300

    with redis

--- src/main/java/com/company/DBOperation.java
+++ src/main/java/com/company/DBOperation.java
@@ -1,57 +1,39 @@
 package com.company;
 
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
-import com.google.common.io.Files;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
 
-public class FileOperation {
-
-    private static final Logger log = LoggerFactory.getLogger(FileOperation.class);
+public class DBOperation {
 
     private static final String redisHost = "localhost";
     private static final Integer redisPort = 6379;
     private static JedisPool pool = null;
-
     Storage storage;
-    public static List<String> urls = Lists.newArrayList();
 
-    public FileOperation(Storage storage) {
+    public DBOperation(Storage storage) {
         this.storage = storage;
         pool = new JedisPool(redisHost, redisPort);
     }
 
-    public void fillFile () {
-        log.info("Started");
-        Iterator i = Storage.getInstance("www.yourShortUrl.com/").urlMap.entrySet().iterator();
+    public void fillDB () {
+        Iterator i = Storage.urlMap.entrySet().iterator();
         Set<String> keys = Storage.secretKeys.keySet();
+
         for (String key : keys) {
-            log.info("Keys checking");
             while (i.hasNext()) {
-                log.info("while");
                 Map.Entry pairs = (Map.Entry) i.next();
                 String entryKey = pairs.getKey().toString();
                 String entryValue = pairs.getValue().toString();
-                log.info(key);
-                log.info(pairs.getValue().toString());
                 HashMap<String, String> forDB = new HashMap<>();
                 forDB.put(entryKey, entryValue);
+
                 if(key.equals(pairs.getValue().toString())) {
-                    log.info("Equals");
                     Jedis jedis = pool.getResource();
                     try {
                         jedis.hmset(Storage.secretKeys.get(key), forDB);
-                       log.info("Saved");
                     } catch (JedisException e) {
                         e.printStackTrace();
                     }
@@ -60,21 +42,34 @@ public class FileOperation {
         }
     }
 
-//    public HashMap<String, String> readFromFile() {
-//        HashMap<String, String> mapFile = new HashMap<>();
-//        if (file.isFile()) {
-//            try {
-//                urls = Files.readLines(file, Charsets.UTF_8);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            for (int i = 0; i < urls.size(); i++) {
-//                String str = urls.get(i).split(" = ", 2)[0];
-//                String str1 = urls.get(i).split(" = ", 2)[1];
-//                mapFile.put(str, str1);
-//            }
-//        }
-//        return mapFile;
-//    }
+    public HashMap<String, String> readFromDB(String key) {
+        HashMap<String, String> mapDB = new HashMap<>();
+
+        Jedis jedis = pool.getResource();
+        try {
+            Map<String, String> retrieveMap = jedis.hgetAll(key);
+            Iterator i = retrieveMap.entrySet().iterator();
+            while (i.hasNext()) {
+                Map.Entry pairs = (Map.Entry) i.next();
+                String entryKey = pairs.getKey().toString();
+                String entryValue = pairs.getValue().toString();
+                mapDB.put(entryKey, entryValue);
+            }
+        } catch (JedisException e) {
+            e.printStackTrace();
+        }
+        return mapDB;
+    }
+
+    public String deleteFromDB(String key) {
+        String result = "This key is not correct";
+        Jedis jedis = pool.getResource();
+        try {
+            jedis.del(key);
+            result = "Your URLs are successfully deleted";
+        } catch (JedisException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
--- src/main/java/com/company/URLModification.java
+++ src/main/java/com/company/URLModification.java
@@ -5,26 +5,12 @@ import com.google.common.hash.Hashing;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 
-public class URLModification implements Encoder, Decoder {
-
-    private FileOperation fileReader = new FileOperation(new File(System.getProperty("user.home") + "/URL.txt"), Storage.getInstance("www.yourShortUrl.com/"));
+public class URLModification implements Encoder {
 
     @Override
     public String encode(String longUrl) {
         String shortURL = Hashing.sha256().hashString(longUrl, StandardCharsets.UTF_8).toString();
 
         return shortURL;
-    }
-
-    @Override
-    public String decode(String shortUrl) {
-        String s = "There isn't such URL";
-        for (int i = 0; i < fileReader.urls.size(); i++) {
-            String str = fileReader.urls.get(i).split(" = ", 2)[0];
-            if (shortUrl.equals(str)) {
-                s = fileReader.urls.get(i).split(" = ", 2)[1];
-            }
-        }
-        return s;
     }
 }
